7c36e3bbec402180447192c6450119e1cf96cb26
Waleed Saad <wldsaad.alx@gmail.com>
2023-02-16 11:48:30 -0800

    Add documentation for 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/* This is the main entry point function.
+ * Prints \"Programming is like building a multilingual puzzle.
+ * Return successfully
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
