f8246aa56d5ddc9e70a325e4a673067a4b08fc3a
Waleed Saad <wldsaad.alx@gmail.com>
2023-02-17 16:51:52 -0800

    Add 9-print_comb.c file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: '5-print_numbers.c'
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+			putchar(j + '0');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+}
