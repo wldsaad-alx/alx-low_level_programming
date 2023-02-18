265049614b49a9f7a52a3b931fdfdf83a35fddc7
Waleed Saad <wldsaad.alx@gmail.com>
2023-02-17 16:32:39 -0800

    Edit 4-print_alphabt.c file

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,13 +10,13 @@
 int main(void)
 {
 	char letter;
-	
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (letter != 'q' && letter != 'e')
 			putchar(letter);
 	}
-	
+
 	putchar('\n');
 
 	return (0);
